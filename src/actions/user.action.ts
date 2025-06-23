b680610ab1d5b0ddf0940dad8c9b64da53d663fd
ssr0016 <samsonrecluta0016@gmail.com>
2025-06-23 21:49:22 +0800

    third commit

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -2,6 +2,7 @@
 
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export async function syncUser() {
   try {
@@ -61,4 +62,102 @@ export async function getDbUserId() {
   if (!user) throw new Error("User not found");
 
   return user.id;
+}
+
+export async function getRandomUsers() {
+  try {
+    const userId = await getDbUserId();
+
+    if (!userId) return [];
+
+    // get 3 random users excluding ourselves & users that we already follow
+    const randomUsers = await prisma.user.findMany({
+      where: {
+        AND: [
+          {NOT: { id: userId }},
+          {
+            NOT: {
+              followers: {
+                some: {
+                  followerId: userId,
+                },
+              },
+            },
+          },
+        ]
+      },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        image: true,
+        _count: {
+          select: {
+            followers: true,
+          },
+        },
+      },
+      take: 3,
+    });
+
+    return randomUsers;
+  } catch (error) {
+    console.error("Error fetching random users:", error);
+    return [];
+  }
+}
+
+export async function toggleFollow(targetUserId: string) {
+  try {
+    const userId = await getDbUserId();
+
+    if(!userId) return;
+
+    if (userId === targetUserId) throw new Error("You cannot follow yourself");
+
+    const existingFollow = await prisma.follows.findUnique({
+      where: {
+        followerId_followingId: {
+          followerId: userId,
+          followingId: targetUserId,
+        },
+      },
+    })
+
+    if(existingFollow) {
+      // unfollow
+      await prisma.follows.delete({
+        where: {
+          followerId_followingId: {
+            followerId: userId,
+            followingId: targetUserId,
+          },
+        },
+      })
+    } else {
+      // follow
+      await prisma.$transaction([
+        prisma.follows.create({
+          data: {
+            followerId: userId,
+            followingId: targetUserId,
+          },
+        }),
+
+        prisma.notification.create({
+          data: {
+            type: "FOLLOW",
+            userId: targetUserId, // user being followed
+            creatorId: userId, // user following
+          }
+        })
+      ])
+    }
+
+    revalidatePath("/"); //  // purge the cache for the home page
+    return { success: true };
+  } catch (error) {
+    console.error("Error toggling follow:", error);
+    return { success: false, error: "Error toggling follow" };
+  }
 }
---
+++ src/components/PostCard.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import { useState } from "react";
+
+import { useUser } from "@clerk/nextjs";
+import { getPosts } from "@/actions/post.action";
+
+import { toggleLike } from "@/actions/post.action";
+
+type Posts = Awaited<ReturnType<typeof getPosts>>;
+type Post = Posts[number];
+
+function PostCard({ post, dbUserId }: { post: Post, dbUserId: string | null }) {
+  const { user } = useUser();
+  const [newComment, setNewComment] = useState("");
+  const [isCommenting, setIsCommenting] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [hasLiked, setHasLiked] = useState(post.likes.some((like) => like.userId === dbUserId));
+  const [optimisticLikes, setOptimisticLikes] = useState(post._count.likes);
+  
+
+
+
+  const handleLike = async () => {
+    if(isLiking) return;
+      
+    try {
+      setIsLiking(true);
+      setHasLiked((prev) => !prev);
+      setOptimisticLikes((prev) => prev + (hasLiked ? -1 : 1));
+      await toggleLike(post.id);
+    } catch (error) {
+      setOptimisticLikes(post._count.likes);
+      setHasLiked(post.likes.some((like) => like.userId === dbUserId));
+    }finally {
+      setIsLiking(false);
+    }
+  }
+
+  const handleAddComment = async () => {}
+
+  const handleDelete = async () => {}
+
+ 
+  try {
+
+  } catch (error) {
+    console.log("Failed to create post: ", error);
+  }
+
+  return (
+    <div>PostCard</div>
+  )
+}
+
+export default PostCard
