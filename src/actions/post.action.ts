1ab3b63b4d97515be9e4fec3c79640210d99d061
ssr0016 <samsonrecluta0016@gmail.com>
2025-06-25 21:53:22 +0800

    fifth commit

--- src/actions/post.action.ts
+++ src/actions/post.action.ts
@@ -37,6 +37,7 @@ export async function getPosts() {
       include: {
         author: {
           select: {
+            id: true,
             name: true,
             image: true,
             username: true,
